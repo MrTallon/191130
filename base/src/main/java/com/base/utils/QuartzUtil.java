1d4af66cfcceed88c791509b8e766b77e009cccc
tallon <talloner@qq.com>
2019-12-03 00:19:59 +0800

    quartz

--- base/src/main/java/com/base/utils/QuartzUtil.java
+++ base/src/main/java/com/base/utils/QuartzUtil.java
@@ -1,5 +1,11 @@
 package com.base.utils;
 
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 /**
  * 定时任务工具类
  *
@@ -7,6 +13,11 @@ package com.base.utils;
  * @version v1.0.0
  * @date 2019-12-02 23:47
  */
+@Component
 public class QuartzUtil {
+    @Scheduled(cron = "0/5 * * * * ? ")
+    public void printCurrentTime() {
+        System.out.println(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()));
+    }
 
 }
